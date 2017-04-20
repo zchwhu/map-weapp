04d473151f833a221e32c82aeb51b5918502a58c
zch0301 <291053953@qq.com>
2017-04-20 20:27:16 +0800

    添加数据绑定，接入后台数据

--- pages/detail/detail.js
+++ pages/detail/detail.js
@@ -1,8 +1,44 @@
 // pages/detail/detail.js
+var app = getApp();
 Page({
-  data: {},
+  data: {
+    location:{}
+  },
   onLoad: function (options) {
     // 页面初始化 options为页面跳转所带来的参数
+    var locaId = options.id;
+    var baseUrl = app.globalData.baseUrl;
+    var that = this;
+    wx.request({
+      url: baseUrl+'HerPinkMap/Anon/locaDetail?locaId='+locaId,
+      data: {},
+      method: 'GET', // OPTIONS, GET, HEAD, POST, PUT, DELETE, TRACE, CONNECT
+      // header: {}, // 设置请求的 header
+      success: function(res){
+        // success
+        console.log(res.data);
+        var locaData = res.data;
+        that.processLocaData(locaData);
+      },
+      fail: function() {
+        // fail
+      },
+      complete: function() {
+        // complete
+      }
+    })
+  },
+  processLocaData: function(data){
+    var locationInfo = {};
+    locationInfo.name = data['名称'];
+    locationInfo.position = data['地址'];
+    locationInfo.phonenum = data['电话'];
+    locationInfo.introduction = data['描述'];
+    locationInfo.latitude = data['纬度'];
+    locationInfo.longitude = data['经度'];
+    this.setData({
+      location: locationInfo
+    })
   },
   previewImg: function (e) {
     wx.previewImage({
@@ -19,16 +55,20 @@ Page({
       }
     })
   },
-  callTel: function (e) {
+  callTel: function (event) {
+    var phonenum = event.currentTarget.dataset.phonenum;
     wx.makePhoneCall({
-      phoneNumber: '1340000' 
+      phoneNumber: phonenum 
     })
   },
 
-  showPos: function(e){
+  showPos: function(event){
+    var locaLati = event.currentTarget.dataset.lati;
+    console.log(locaLati);
+    var locaLong = event.currentTarget.dataset.long;
     wx.openLocation({
-      latitude: 30.529348,
-      longitude: 114.358991,
+      latitude: locaLati,
+      longitude: locaLong,
       scale: 28
     })
   }
--- pages/index/index.js
+++ pages/index/index.js
@@ -1,103 +1,99 @@
 //index.js
 //获取应用实例
-var app = getApp()
+var app = getApp();
 Page({
+  onLoad: function(event){
+    var that = this;
+    wx.request({
+      url: 'http://202.114.70.53:8082/HerPinkMap/Anon/getCates',
+      data: {},
+      method: 'GET', // OPTIONS, GET, HEAD, POST, PUT, DELETE, TRACE, CONNECT
+      // header: {}, // 设置请求的 header
+      success: function(res){
+        // success
+        var type = res.data[0];
+        var firstTypePos = res.data[1];
+        console.log(res.data[0]);
+        that.processTypeData(type);
+        that.getDefaultMarkers(firstTypePos);
+      },
+      fail: function() {
+        // fail
+      },
+      complete: function() {
+        // complete
+      }
+    })
+  },
+
+  // 处理类型数据
+  processTypeData: function(data){
+    var types = [];
+    for(var key in data){
+      var temp = {};
+      temp.id = key;
+      temp.name = data[key][0];
+
+      var subUrl = data[key][1].substring(data[key][1].indexOf('HerPinkMap'));
+      temp.icon = app.globalData.baseUrl + subUrl;
+      types.push(temp);
+    }
+    this.setData({
+      types: types,
+      currentIndex:types[0].id
+    })
+  },
+
+  getDefaultMarkers: function(data){
+    var firstPos = [];
+    var iconPath = '../../images/movie.png'; 
+    for(var key in data){
+      var temp = {
+        width: 25,
+        height: 25,
+        iconPath: iconPath
+      };
+      temp.id = key;
+      temp.latitude = data[key][0];
+      temp.longitude = data[key][1];
+      firstPos.push(temp);
+    }
+    this.setData({
+      markers: firstPos
+    })
+  },
+
+  processCategoryData: function(data){
+    var categoryData = data[0];
+    var positionData = data[1];
+    var positions = [];
+    var iconPath = '../../images/ktv.png'; 
+    for(var key in positionData){
+      var temp = {
+        width: 25,
+        height: 25,
+        iconPath: iconPath
+      };
+      temp.id = key;
+      temp.latitude = positionData[key][0];
+      temp.longitude = positionData[key][1];
+      positions.push(temp);
+    }
+    console.log(positions);
+    this.setData({
+      markers: positions
+    })
+  },
+
   data: {
-    currentIndex: 0,//类别索引值
-    markers: [{
-      iconPath: "/images/movie.png",
-      id: 0,
-      latitude: 30.541093,
-      longitude: 114.360734,
-      width: 20,
-      height: 20
-    },{
-      iconPath: "/images/movie.png",
-      id: 2,
-      latitude: 30.5215084396,
-      longitude: 114.354399389,
-      width: 20,
-      height: 20
-    },{
-      iconPath: "/images/movie.png",
-      id: 3,
-      latitude: 30.529348,
-      longitude: 114.358991,
-      width: 20,
-      height: 20
-    },{
-      iconPath: "/images/movie.png",
-      id: 4,
-      latitude: 30.530568,
-      longitude: 114.359168,
-      width: 20,
-      height: 20
-    },{
-      iconPath: "/images/movie.png",
-      id: 5,
-      latitude: 30.530601,
-      longitude: 114.360450,
-      width: 20,
-      height: 20
-    }],
-    ktv: 
-      [{
-      iconPath: "/images/ktv.png",
-      id: 0,
-      latitude: 30.541093,
-      longitude: 114.360734,
-      width: 20,
-      height: 20
-    },{
-      iconPath: "/images/ktv.png",
-      id: 2,
-      latitude: 30.5215084396,
-      longitude: 114.354399389,
-      width: 20,
-      height: 20
-    },{
-      iconPath: "/images/ktv.png",
-      id: 3,
-      latitude: 30.529348,
-      longitude: 114.358991,
-      width: 20,
-      height: 20
-    },{
-      iconPath: "/images/ktv.png",
-      id: 4,
-      latitude: 30.530568,
-      longitude: 114.359168,
-      width: 20,
-      height: 20
-    },{
-      iconPath: "/images/ktv.png",
-      id: 5,
-      latitude: 30.530601,
-      longitude: 114.360450,
-      width: 20,
-      height: 20
-    }]
+    currentIndex: 1,//类别索引值
+    types: [],
+    markers: []
   },
   markertap(e) {
-    // console.log(e.latitude);
-    // wx.openLocation({
-    //   latitude: 53.324520, // 纬度，范围为-90~90，负数表示南纬
-    //   longitude: 23.21229, // 经度，范围为-180~180，负数表示西经
-    //   scale: 14, // 缩放比例
-    //   // name: 'name', // 位置名
-    //   // address: 'address', // 地址的详细说明
-    //   success: function(res){
-    //     // success
-    //   },
-    //   fail: function() {
-    //     // fail
-    //   },
-    //   complete: function() {
-    //     // complete
-    //   }
-    // })
+    var posId = e.markerId;
     wx.navigateTo({
-      url: '../detail/detail',
+      url: '../detail/detail?id='+posId,
       success: function(res){
         // success
       },
@@ -116,10 +112,29 @@ Page({
   //切换类别
   switchCategory: function(event){
     var that = this;
+    var typeid = event.currentTarget.dataset.typeid;
     var index = event.currentTarget.dataset.index;
+    wx.request({
+      url: 'http://202.114.70.53:8082/HerPinkMap/Anon/getLocas?cateId='+typeid,
+      data: {},
+      method: 'GET', // OPTIONS, GET, HEAD, POST, PUT, DELETE, TRACE, CONNECT
+      // header: {}, // 设置请求的 header
+      success: function(res){
+        // success
+        console.log(res);
+        var data = res.data;
+        that.processCategoryData(data);
+      },
+      fail: function() {
+        // fail
+      },
+      complete: function() {
+        // complete
+      }
+    })
+    console.log(index);
     this.setData({
-      currentIndex: index,
-      markers: that.data.ktv 
+      currentIndex: typeid
     });
   }
 })
