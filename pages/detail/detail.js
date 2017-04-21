25d477c1510bf4b1275f0d5a955051dbe5b5c379
zch0301 <291053953@qq.com>
2017-04-21 18:31:44 +0800

    修复位置打开的问题

--- pages/detail/detail.js
+++ pages/detail/detail.js
@@ -30,20 +30,25 @@ Page({
   },
   processLocaData: function(data){
     var locationInfo = {};
-    locationInfo.name = data['名称'];
-    locationInfo.position = data['地址'];
-    locationInfo.phonenum = data['电话'];
-    locationInfo.introduction = data['描述'];
-    locationInfo.latitude = data['纬度'];
-    locationInfo.longitude = data['经度'];
+    var baseUrl = app.globalData.baseUrl+'HerPinkMap';
+    locationInfo.imgSrc = baseUrl+data.locaPic;
+    locationInfo.name = data.locaName;
+    locationInfo.position = data.locaAddress;
+    locationInfo.phonenum = data.locaPhone;
+    locationInfo.introduction = data.locaDes;
+    locationInfo.latitude = data.locaLatitude;
+    locationInfo.longitude = data.locaLongitude;
     this.setData({
       location: locationInfo
     })
   },
+
+  //图片预览
   previewImg: function (e) {
+    var imgSrc = e.currentTarget.dataset.src;
     wx.previewImage({
-      current: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1489831567103&di=d7e642d220caf424b80ea47a6ef0b65d&imgtype=0&src=http%3A%2F%2Fimg6.ph.126.net%2FOqLHvkuVpoF6m0nYqWPhqQ%3D%3D%2F2527082340925283346.jpg', // 当前显示图片的链接，不填则默认为 urls 的第一张
-      urls: ['https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1489831567103&di=d7e642d220caf424b80ea47a6ef0b65d&imgtype=0&src=http%3A%2F%2Fimg6.ph.126.net%2FOqLHvkuVpoF6m0nYqWPhqQ%3D%3D%2F2527082340925283346.jpg'],
+      current: imgSrc, // 当前显示图片的链接，不填则默认为 urls 的第一张
+      urls: [imgSrc],
       success: function (res) {
         // success
       },
@@ -55,6 +60,8 @@ Page({
       }
     })
   },
+
+  //拨打电话
   callTel: function (event) {
     var phonenum = event.currentTarget.dataset.phonenum;
     wx.makePhoneCall({
@@ -62,6 +69,7 @@ Page({
     })
   },
 
+  //打开内置地图导航
   showPos: function(event){
     var locaLati = event.currentTarget.dataset.lati;
     console.log(locaLati);
